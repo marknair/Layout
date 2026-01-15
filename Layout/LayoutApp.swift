f5feb4b1f924a87eedb5014100752d7e08577bfa
Mark <marknair@me.com>
2026-01-15 09:21:28 -0600

    Initial Commit

---
+++ Layout/LayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutApp.swift
+//  Layout
+//
+//  Created by Mark Nair on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct LayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
